eaab5f6cf9686dafae912c66ac8645f93341676c
Willy <wrosahua@everis.com>
2021-06-09 22:30:19 -0500

    feat: realizar su tarea aca

--- src/App.js
+++ src/App.js
@@ -26,67 +26,19 @@ function miPromesa(numero) {
 function App() {
   const [data, setData] = useState(null)
 
-  const [rptaPromise1, setRptaPromise1] = useState("")
-  const [rptaPromise2, setRptaPromise2] = useState("")
-    
-
   useEffect(()=> {
-
     //PROMESAS
-    miPromesa(3).
-              then((data) => {
-                console.log(data)
-                setRptaPromise1(data)
-              }).catch((data)=> {
-                console.log(data)
-                setRptaPromise1(data)
-              }).finally((data)=> {
-                console.log(data)
-              })
-
-    async function pruebaPromesa() {
-      try {
-        const promesa2 = await miPromesa(10)
-        console.log("promesa 2")
-        console.log(promesa2)
-        setRptaPromise2(promesa2)
-      } catch(error) {
-        setRptaPromise2(error)
-        console.log(error)
-      }
-    } 
-
-
-    pruebaPromesa()
-    
-
-    //llamar a la api 
-    fetch('https://hn.algolia.com/api/v1/search?query=redux')
-        .then((response) => response.json())
-        .then((data) => {
-          //console.log(data)
-          //setData(data)
-        })
-
-    async function traerDataPe() {
-      const dataFromApiJSON = await fetch('https://hn.algolia.com/api/v1/search?query=redux')
-      try {
-        const dataReal = await dataFromApiJSON.json()
-        //console.log(dataReal)
-        //setData(dataReal)
-      } catch(error) {
-        console.log(error)
-      }
-    }
-
     async function searchBook() {
       const response = await axios('https://hn.algolia.com/api/v1/search?query=java')
       console.log(response.data)
       setData(response.data)
     }
 
-    traerDataPe()
     searchBook()
+
+    return ()=> {
+      //limpiar aqui - componentWillUnMount
+    }
 
     
   }, [])
